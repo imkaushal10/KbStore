ed64c152e703b57c69ff960e972a16d16a24f2c0
Kaushal Bhattarai <kb147181@gmail.com>
2025-12-23 12:14:59 -0600

    Add EmailInput component for user email sign-up

---
+++ components/EmailInput.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function EmailInput() {
+
+    return (
+        <div className="sign-up">
+            <input placeholder="Email address..." />
+            <button>Sign Up</button>
+        </div>
+    )
+}
