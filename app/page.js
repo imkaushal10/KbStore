5d2c0bb7c718976c8f597a67791678ce9bdc3cb1
Kaushal Bhattarai <kb147181@gmail.com>
2025-12-24 15:16:45 -0600

    Update Home component to display welcome message and description

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      kaushal hello!!!!
+      <h1>Welcome to KBstore!</h1>
+      <p>Your one-stop shop for all things cool and productive.</p>
     </div>
   );
 }
