9648cfa05c8bb0d258a91a3d9e5935db534d52a7
Kaushal Bhattarai <kb147181@gmail.com>
2026-01-02 19:24:54 -0600

    Add Products component with initial structure and description

---
+++ components/Products.jsx
@@ -0,0 +1,9 @@
+export default function Products() {
+    return (
+        <div className="products-page">
+            <h2>Our Products</h2>
+            <p>Explore our wide range of products designed for programmers and productivity enthusiasts.</p>
+            {/* Product listings would go here */}
+        </div>
+    );
+}
