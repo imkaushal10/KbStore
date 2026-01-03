6f49ef5e2c699f3980bc609ece5c9f826b037e25
Kaushal Bhattarai <kb147181@gmail.com>
2026-01-02 19:30:24 -0600

    Refactor Home component to include ImageBanner and Products components

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
+import ImageBanner from "@/components/ImageBanner";
+import Products from "@/components/Products";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to KBstore!</h1>
-      <p>Your one-stop shop for all things cool and productive.</p>
-    </div>
+    <>
+      <ImageBanner />
+      <section>
+        <Products />
+      </section>
+    </>
   );
 }
