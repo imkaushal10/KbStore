4d6863be062e08e9f27b7ef8e5a7b709fe2bf95e
Kaushal Bhattarai <kb147181@gmail.com>
2026-01-09 15:47:24 -0600

    Refactor getProducts function to return stickers and planner data, and update Home component to pass them to Products

--- app/page.js
+++ app/page.js
@@ -5,19 +5,21 @@ export async function getProducts() {
   
   const response = await fetch('http://localhost:3000/api/products')
   const products = await response.json()
-  return products
+   const stickers = []; // Your stickers data
+  const planner = {}; // Your planner data
+  return { products, stickers, planner };
   
 }
 
 export default async function Home() {
-  const products = await getProducts()
+  const {products, stickers, planner } = await getProducts()
   console.log(products)
 
   return (
     <>
       <ImageBanner />
       <section>
-        <Products />
+        <Products stickers={stickers} planner={planner} />
       </section>
     </>
   );
